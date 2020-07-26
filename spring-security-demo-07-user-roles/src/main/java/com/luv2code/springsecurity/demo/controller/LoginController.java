b34965212d4cb111b34d3fc20e42cd47165e98a7
Nitin6643 <68808626+Nitin6643@users.noreply.github.com>
2020-07-26 16:31:23 +0530

    syso added

--- spring-security-demo-07-user-roles/src/main/java/com/luv2code/springsecurity/demo/controller/LoginController.java
+++ spring-security-demo-07-user-roles/src/main/java/com/luv2code/springsecurity/demo/controller/LoginController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class LoginController {
 	@GetMapping("/showMyLoginPage")
 	public String showMyLoginPage() {
-		
+		System.out.println("Welcome you are in login page");
 //		return "plain-login";
 		return "fancy-login";
 	}
