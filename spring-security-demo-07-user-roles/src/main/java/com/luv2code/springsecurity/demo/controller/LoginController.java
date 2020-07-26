2aca2b19e418576a7b62b4ce3f8bfb4804bc6a4e
Nitin.Sharma <Nitin.Sharma@C2L-L-133.C2LBIZMUM.COM>
2020-07-27 00:46:44 +0530

    created mapping for error page

--- spring-security-demo-07-user-roles/src/main/java/com/luv2code/springsecurity/demo/controller/LoginController.java
+++ spring-security-demo-07-user-roles/src/main/java/com/luv2code/springsecurity/demo/controller/LoginController.java
@@ -10,4 +10,12 @@ public class LoginController {
 //		return "plain-login";
 		return "fancy-login";
 	}
+	
+	
+	// add request mapping for /access-denied
+	@GetMapping("/access-denied")
+	public String showAccessDenied() {
+		return "access-denied";
+	}
+	
 }
